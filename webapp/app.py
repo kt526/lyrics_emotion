d7c76cf59460770204d89db61fb5375bba7f90c3
kt526 <132878454+kt526@users.noreply.github.com>
2024-07-10 15:10:41 +0800

    Add files

---
+++ webapp/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+
+from flask_cors import CORS
+from predict import *
+
+app = Flask(__name__)   
+CORS(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/process_lyrics', methods=['POST'])
+def process_lyrics():
+    data = request.get_json()
+
+    # Extract model_k and lyrics_k from the received JSON data
+    raw_lyrics = data.get('lyrics')
+
+    # Perform processing or analysis here (replace this with your actual processing logic)
+    print(f"Processing lyrics using model: {raw_lyrics}")
+
+    emotion = predict_emotion(raw_lyrics)
+
+    # load the model from disk
+    # Return the processed result as JSON
+    return jsonify({'data': emotion})
+
+if __name__ == "__main__":
+    app.run(debug=True)
