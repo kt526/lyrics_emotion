d7ac7a4c08350e39acd7deba9a2c1acd478bb8e6
kt526 <132878454+kt526@users.noreply.github.com>
2024-07-16 23:23:27 +0800

    Delete files

--- process.py
+++
@@ -1,9 +0,0 @@
-import requests as rq
-from datetime import datetime
-
-BASE_URL = 'https://adminapi.pythonanywhere.com/'
-
-payload = {'input': 'hello!'}
-response = rq.get(BASE_URL, params=payload)
-
-json_values = response.json()
